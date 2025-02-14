a633038645d249c61768025309b76b46110a9ac8
Your Name <you@example.com>
2025-02-14 16:54:51 -0500

    Created RedditData.py which RedditDataManager class. This will act as the data manager and has a function to combine posts and all their associated comments, returning them one thread at a time.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,8 @@
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 from flask_login import UserMixin
 
 from app.extensions import db
@@ -16,3 +19,15 @@ class Users(UserMixin, db.Model):
     #email = db.Column(db.String(100), nullable=False, unique=True)
 
 #search history model class
+
+# unimplemented RedditData model class. 
+class RedditData(Base):
+    __tablename__ = 'redditData'
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    author = db.Column(db.String(100), nullable=False)
+    created_utc = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.String(100), nullable=False)
+    link_id = db.Column(db.String(100), nullable=False,)
+    
+    def get_id(self):
+        return str(self.id)
