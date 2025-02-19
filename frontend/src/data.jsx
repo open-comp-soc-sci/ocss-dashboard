0b3fef3ed1f415b557e292bbb34f815804d9a920
unknown <erictruong4402@gmail.com>
2025-02-18 22:52:43 -0500

    Added routing and removed unecessary python files

---
+++ frontend/src/data.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Data() {
+    //make fetch api call to postgres
+    //const handleSearch
+    //const removeSearch
+    //const clearAllSearch
+
+  return (
+    <div className="container mt-5">
+      <div className="text-center">
+        <h1>Data Page</h1>
+        <p>This is where the searches and results will be displayed. (Search History Also?)</p>
+      </div>
+    </div>
+  );
+}
+
+export default Data;
